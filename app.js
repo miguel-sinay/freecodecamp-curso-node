ed2e7f7f6979dfb976331a2ba90891f6ed3d4bac
Miguel Sinay <mmviniciosinay@gmail.com>
2024-04-25 23:55:39 -0600

    Módulo os

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-console.log(process.platform)
-console.log(process.env)
-console.log(process.argv)
-// [node, app.js, 6, 7]
-//   0      1     2  3
-console.table(process.argv)
-for (let i = 2; i < process.argv.length; i++) {
-    console.log(process.argv[i])
-}
+const os = require('os')
+console.log(os.type())
+console.log(os.homedir())
+console.log(`Este sistema operativo ha estado encendido por ${os.uptime()} s`)
+console.log(os.userInfo())
