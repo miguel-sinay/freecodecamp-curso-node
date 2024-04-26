e5b5d36b89469958602338458794e14681d23841
Miguel Sinay <mmviniciosinay@gmail.com>
2024-04-25 23:48:10 -0600

    Módulo process

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-console.log('¡Hola, Mundo!')
-console.warn('[ADVERTENCIA] Ten esto en mente:')
-console.error(new Error('¡Ocurrió un ERROR CRITICO!'))
+console.log(process.platform)
+console.log(process.env)
+console.log(process.argv)
+// [node, app.js, 6, 7]
+//   0      1     2  3
+console.table(process.argv)
+for (let i = 2; i < process.argv.length; i++) {
+    console.log(process.argv[i])
+}
