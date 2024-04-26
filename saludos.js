8d0140798280c468da2f8bc386c71774e976633f
Miguel Sinay <mmviniciosinay@gmail.com>
2024-04-25 23:01:50 -0600

    Exportación mediante objeto

--- saludos.js
+++ saludos.js
@@ -6,5 +6,7 @@ function saludarHolaMundo() {
   return '¡Hola, Mundo!'
 }
 
-module.exports.saludar = saludar
-module.exports.saludarHolaMundo = saludarHolaMundo
+module.exports = {
+  saludar: saludar,
+  saludarHolaMundo: saludarHolaMundo
+}
