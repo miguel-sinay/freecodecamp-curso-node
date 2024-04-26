1e4e29d5c4ae3a716c4fb19af524d13bacd1304f
Miguel Sinay <mmviniciosinay@gmail.com>
2024-04-26 14:16:24 -0600

    Ejecución de métodos de forma sincrónica sin otras funciones como argumentos

--- app.js
+++ app.js
@@ -2,36 +2,22 @@ const fs = require('fs')
 
 // Lectura de archivo
 console.log('Antes de leer el archivo')
-fs.readFileSync('index.html', 'utf-8', (err, contenido) => {
-  if (err) throw err
-  console.log('Archivo leído')
-})
+const archivo = fs.readFileSync('index.html', 'utf-8')
+console.log(archivo)
 console.log('Después de leer el archivo')
 
 // Cambiar nombre de archivo
-fs.renameSync('index.html', 'main.html', (err) => {
-  if (err) throw err
-  console.log('Nombre cambiado exitosamente')
-})
+fs.renameSync('index.html', 'main.html')
 console.log('Después de cambiar nombre al archivo')
 
 // Agregación de contenido al archivo
-fs.appendFileSync('index.html', '<p>Hola</p>', (err) => {
-  if (err) throw err
-  console.log('Archivo actualizado')
-})
+fs.appendFileSync('index.html', '<p>Hola</p>')
 console.log('Después de agregar contenido al archivo')
 
 // Reemplazo de contenido del archivo
-fs.writeFileSync('index.html', 'Contenido nuevo', (err) => {
-  if (err) throw err
-  console.log('Contenido reemplazado satisfactoriamente')
-})
+fs.writeFileSync('index.html', 'Contenido nuevo')
 console.log('Después de reemplazar contenido del archivo')
 
 // Eliminación de archivo
-fs.unlinkSync('main.html', (err) => {
-  if (err) throw err
-  console.log('Archivo eliminado')
-})
+fs.unlinkSync('main.html')
 console.log('Después de eliminar archivo')
