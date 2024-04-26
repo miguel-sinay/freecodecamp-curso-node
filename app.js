e63894142d683b46e3e2f4abdf71bd6be4900505
Miguel Sinay <mmviniciosinay@gmail.com>
2024-04-25 23:12:38 -0600

    Módulo console

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const { saludar, saludarHolaMundo } = require("./saludos.js")
-
-console.log(saludo.saludarHolaMundo())
-console.log(saludar('freeCodeCamp'))
+console.log('¡Hola, Mundo!')
+console.warn('[ADVERTENCIA] Ten esto en mente:')
+console.error(new Error('¡Ocurrió un ERROR CRITICO!'))
