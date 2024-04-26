a58cf20180af2be4eed904a9d95477853df16a67
Miguel Sinay <mmviniciosinay@gmail.com>
2024-04-26 12:56:40 -0600

    Detener programa si se detecta error

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const fs = require('fs')
 
 fs.readFile('index.html', 'utf-8', (err, contenido) => {
-  if (err) {
-    console.error(err)
-    // O también se puede emplear throw err.
-    // Considerar que este detiene la ejecución completa
-  }
-  else console.log(contenido)
-  console.log('Mensaje...')
+  if (err) throw err
+  console.log(contenido)
 })
