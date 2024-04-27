1593e3509cb02076ef286ac7cad0fe1087723b8b
Miguel Sinay <mmviniciosinay@gmail.com>
2024-04-27 16:48:55 -0600

    Objeto a cadena de texto y parseo

--- json-demo.js
+++ json-demo.js
@@ -1,2 +1,28 @@
-const curso = require('./curso.json') // se importa como un objeto de JS
-console.log(curso.temas)
+let infoCurso = {
+  "titulo": "Aprende Node.js",
+  "numVistas": 45642,
+  "numLikes": 21123,
+  "temas": [
+    "JavaScript",
+    "Node.js"
+  ],
+  "esPublico": true
+}
+console.log(typeof infoCurso)
+// Hasta aquí es un objeto de JavaScript en la memoria de la computadora,
+// no es un texto adecuado para intentar transmitirlo a un servidor
+
+
+// Objeto -> Cadena de caracteres en formato JSON
+let infoCursoJSON = JSON.stringify(infoCurso)
+console.log(infoCursoJSON)
+console.log(typeof infoCursoJSON)
+// Ahora sí es una cadena de texto en formato JSON apta para su transmisión
+
+// Cadena de caracteres -> Objeto
+let infoCursoObjeto = JSON.parse(infoCursoJSON)
+console.log(infoCursoObjeto)
+console.log(typeof infoCursoObjeto)
+// Esto es util en caso de recibir texto en formato JSON de un servidor
+// y sea necesario convertirla a objeto de JavaScript
+console.log(infoCursoObjeto.titulo)
