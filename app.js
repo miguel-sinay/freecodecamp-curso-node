0fc9b718ec8c5b770b6787be2ce3c87114e5d36c
Miguel Sinay <mmviniciosinay@gmail.com>
2024-04-28 21:22:39 -0600

    Chaining promises

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-const estatusPedido = () => {
-  return Math.random() < 0.8
-}
-
-const miPedidoDePizza = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (estatusPedido()) {
-      resolve('¡Pedido exitoso! Su pizza está en camino')
-    } else {
-      reject('Ocurrió un error. Por favor intente nuevamente')
-    }
-  }, 3000);
-})
-
-const manejarPedido = (mensajeDeConfirmacion) => {
-  console.log(mensajeDeConfirmacion)
-}
-
-const rechazarPedido = (mensajeDeError) => {
-  console.log(mensajeDeError)
-}
-
-miPedidoDePizza.then(manejarPedido, rechazarPedido)
-
-// Alternativa más compacta
-miPedidoDePizza
-  .then((mensajeDeConfirmacion) => {
-    console.log(mensajeDeConfirmacion)
+function ordenarProducto(producto) {
+  return new Promise((resolve, reject) => {
+    console.log(`Ordenando: ${producto} de freeCodeCamp`)
+    setTimeout(() => {
+      if (producto === 'taza') {
+        resolve('Ordenando una taza con el logo de freeCodeCamp...')
+      } else {
+        reject('Este producto no está disponible actualmente.')
+      }
+    }, 2000);
   })
-  .catch((mensajeDeError) => {
-    console.log(mensajeDeError)
-  })
-  // Nótese que aquí se practica el Encadenamiento de Métodos (Method Chaining)
+}
 
-  // Y si se quiere compactar aún más, aprovechar los métodos manejadores de arriba
-  miPedidoDePizza.then(manejarPedido).catch(rechazarPedido)
+function procesarPedido(respuesta) {
+  return new Promise(resolve => {
+    console.log('Procesando respuesta...')
+    console.log(`La respuesta fue: "${respuesta}"`)
+    setTimeout(() => {
+      resolve('Gracias por tu compra. Disfruta tu producto')
+    }, 4000);
+  })
+}
+
+// Chaining Promises
+ordenarProducto('taza')
+  .then(respuesta => {
+    console.log('Respuesta recibida')
+    console.log(respuesta)
+    return procesarPedido(respuesta)
+  })
+  .then(respuestaProcesada => {
+    console.log(respuestaProcesada)
+  })
+  .catch(error => {
+    console.log(error)
+  })
