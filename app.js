fa8248d793ec57eb26d2e5d60de9a3cd9f6ebafa
Miguel Sinay <mmviniciosinay@gmail.com>
2024-05-03 08:41:56 -0600

    Response

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const http = require('http')
 
 const servidor = http.createServer((req, res) => {
-  console.log('===> req (solicitud)')
-  console.log(req.url)
-  console.log(req.method)
-  console.log(req.headers)
+  console.log('===> res (solicitud)')
+  console.log(res.getHeaders())
+  console.log(res.statusCode)
+  res.setHeader('content-type', 'application/json')
+  console.log(res.getHeaders())
   res.end('Hola mundo')
 })
 
