8b2c74251a98e17ee4157f654cf89c116166bf4f
Miguel Sinay <mmviniciosinay@gmail.com>
2024-05-03 16:58:01 -0600

    Routing en Node

--- app.js
+++ app.js
@@ -1,11 +1,73 @@
-const miURL = new URL('https://www.ejemplo.org/cursos/programacion?ordenar=vistas&nivel=1')
+const http = require('http')
+const {infoCursos} = require('./cursos')
 
-console.log(miURL.hostname) // www.ejemplo.org
-console.log(miURL.pathname) // /cursos/programacion
+const servidor = http.createServer((req, res) => {
+  const {method} = req;
+  switch(method) {
+    case 'GET':
+      return manejarSolicitudGET(req, res)
+    case 'POST':
+      return manejarSolicitudPOST(req, res)
+    case 'PUT':
+      return manejarSolicitudPUT(req, res)
+    default:
+      res.statusCode = 501
+      return res.end(`El método no puede ser manejado por el servidor: ${method}`)
+  }
+})
 
-console.log(miURL.searchParams)
-console.log(typeof miURL.searchParams)
-console.log(miURL.searchParams.get('ordenar'))
-console.log(miURL.searchParams.get('nivel'))
+function manejarSolicitudGET(req, res) {
+  const path = req.url
 
-console.log(miURL.protocol)
+  console.log(req.statusCode) // 200 OK Asignado por defecto
+
+  if(path === '/') {
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    return res.end('Bienvenidos a mi primer servidor y API creados con Node.js')
+  } else if (path === '/cursos') {
+    res.setHeader('content-type', 'application/json')
+    return res.end(JSON.stringify(infoCursos))
+  } else if (path === '/cursos/programacion') {
+    return res.end(JSON.stringify(infoCursos.programacion))
+  } else if (path === '/cursos/matematicas') {
+    return res.end(JSON.stringify(infoCursos.matematicas))
+  }
+
+  res.statusCode = 404
+  return res.end('El recurso solicitado no existe.')
+}
+
+function manejarSolicitudPOST(req, res) {
+  const path = req.url
+
+  if (path === '/cursos/programacion') {
+    res.statusCode = 201 // Nuevo recurso creado
+    let cuerpo = ''
+    req.on('data', contenido => {
+      cuerpo += contenido.toString()
+    })
+    req.on('end', () => {
+      console.log(cuerpo)
+      console.log(typeof cuerpo)
+
+      cuerpo = JSON.parse(cuerpo)
+      console.log(typeof cuerpo)
+      console.log(cuerpo.titulo)
+      res.end(`El servidor recibió una solicitud ${req.method} para ${path}`)
+    })
+    //return res.end(`El servidor recibió una solicitud ${req.method} para ${path}`)
+  }
+}
+
+function manejarSolicitudPUT(req, res) {
+  const path = req.url
+
+  if (path === '/cursos/programacion') {
+    return res.end(`El servidor recibió una solicitud ${req.method} para ${path}`)
+  }
+}
+
+const PUERTO = 3000
+servidor.listen(PUERTO, () => {
+  console.log(`El servidor está escuchando en el puerto ${PUERTO}...`)
+})
